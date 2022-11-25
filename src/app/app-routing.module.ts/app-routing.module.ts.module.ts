87ae73ef747cd7fb5a61f86792720a9c32f5e9da
J. Wagner <aguswagnercalvo@hotmail.com>
2022-11-25 12:41:03 -0300

    v0.2

--- src/app/app-routing.module.ts/app-routing.module.ts.module.ts
+++ src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "src/app/components/login/login.component"
+
 const routes: Routes = [
+  { path: 'login', component: LoginComponent},
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module.ts/app-routing.module.ts.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
