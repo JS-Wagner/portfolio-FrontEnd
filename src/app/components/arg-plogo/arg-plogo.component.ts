fc031240b574fe6263263e1525e77320a062ca0e
J. Wagner <aguswagnercalvo@hotmail.com>
2022-12-28 14:31:46 -0300

    v0.3
    Correcciones en LoginComponent e implementación del Routing

--- src/app/components/arg-plogo/arg-plogo.component.ts
+++ src/app/components/arg-plogo/arg-plogo.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-arg-plogo',
   templateUrl: './arg-plogo.component.html',
   styleUrls: ['./arg-plogo.component.css']
 })
-export class ArgPLogoComponent {
+export class ArgPLogoComponent implements OnInit {
+  constructor(private router: Router) {}
 
-}
+  ngOnInit(): void {
+  }
+    login() {
+      this.router.navigate(['/login']);
+    }
+  }
